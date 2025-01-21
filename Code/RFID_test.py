abfc95d6598cb3138a1317e16a0751334800f448
Vít Moulis <moulisvit@gmail.com>
2025-01-21 11:17:41 +0100

    2

--- Code/RFID_test.py
+++ Code/RFID_test.py
@@ -1,19 +1,10 @@
-import serial
+from machine import UART, Pin
+import time
 
-ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+uart = UART(1, 9600)
+uart.init(9600, bits=8, parity=None, stop=1, rx=15, tx=14)
 
 while True:
-    string = ser.read(12) 
-
-    if len(string) == 0:
-        print("Please insert a tag")
-        continue
-    else:
-        string = string[1:11] #exclude start x0A and stop x0D bytes
-
-        if string == '0415DB18A3': 
-            print ("You used your black tag")
-        elif string == '0F03028F57':
-            print ("You used your white tag")
-        else:
-            print ("You do not have a valid tag")
+    b = uart.read()
+    if not b == None:
+        print(b)
