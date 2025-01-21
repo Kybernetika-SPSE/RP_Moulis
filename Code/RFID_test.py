b1b581e75c23a28b0017133aeaa330e94f2a8f8c
Vít Moulis <moulisvit@gmail.com>
2025-01-21 13:38:24 +0100

    5

--- Code/RFID_test.py
+++ Code/RFID_test.py
@@ -1,28 +1,14 @@
-#import serial
-from mfrc522 import MFRC522
-import utime
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
 
-#ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
-reader = MFRC522()
-print("Bring TAG closer...")
-print("")
-while True:
-    reader.init()
-    (stat, tag_type) = reader.request(reader.REQIDL)
-    if stat == reader.OK:
-        (stat, uid) = reader.SelectTagSN()
-        if stat == reader.OK:
-            card = int.from_bytes(bytes(uid),"little",False)
-            print("CARD ID: "+str(card))
-utime.sleep_ms(500) 
-while True:
-    
-    tring = ser.read(12) 
-
-    if len(string) == 0:
-        print("Please insert a tag")
-        continue
-    else:
-        string = string[1:11] #exclude start x0A and stop x0D bytes
-
-        print(string)
+try:
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
