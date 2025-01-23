cb15944bb65abe15c1cdb9930a445c30423f8f26
Vít Moulis <moulisvit@gmail.com>
2025-01-23 20:51:00 +0100

    deviceid

--- Talaexe/spotifyTest.py
+++ Talaexe/spotifyTest.py
@@ -3,9 +3,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 
-DEVICE_ID="YOUR_DEVICE_ID"
-CLIENT_ID="YOUR_CLIENT_ID"
-CLIENT_SECRET="YOUR_CLIENT_SECRET"
+DEVICE_ID="35ccd4ae2cab8870610afd219d13b41e84688587"
+CLIENT_ID="5984261fa2d845b3bcf6463bb1df2c97"
+CLIENT_SECRET="9c2280c3c0ae4d9392a8870b90165b91"
 
 # Spotify Authentication
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
@@ -18,4 +18,4 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
 sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
 
 # Play the spotify track at URI with ID 45vW6Apg3QwawKzBi03rgD (you can swap this for a diff song ID below)
-sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:45vW6Apg3QwawKzBi03rgD'])
+sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:2XVQdI3m0giGxNrwUhV3yP'])
