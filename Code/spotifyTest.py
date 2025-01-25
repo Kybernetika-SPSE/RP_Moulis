1c9c4801bf2a0ac7c901d12f2d432dd5ab9bb7a7
Vít Moulis <moulisvit@gmail.com>
2025-01-25 20:01:11 +0100

    spotify

--- Code/spotifyTest.py
+++ Code/spotifyTest.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 from time import sleep
 
 DEVICE_ID="35ccd4ae2cab8870610afd219d13b41e84688587"
-CLIENT_ID="5984261fa2d845b3bcf6463bb1df2c97"
-CLIENT_SECRET="9c2280c3c0ae4d9392a8870b90165b91"
 
 # Spotify Authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                client_secret=CLIENT_SECRET,
-                                                redirect_uri="http://localhost:8080",
-                                                scope="user-read-playback-state,user-modify-playback-state"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 
 
 # Transfer playback to the Raspberry Pi if music is playing on a different device
