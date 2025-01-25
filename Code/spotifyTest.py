b815096d0788deeca2054f5f24676d443ab2eb74
Vít Moulis <moulisvit@gmail.com>
2025-01-25 23:00:47 +0100

    spotifyTest

--- Code/spotifyTest.py
+++ Code/spotifyTest.py
@@ -2,8 +2,10 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 
+DEVICE_ID="35ccd4ae2cab8870610afd219d13b41e84688587"
 scope = "user-read-playback-state,user-modify-playback-state"
 sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
 
+sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
 # Change track
-sp.start_playback(uris=['spotify:track:6gdLoMygLsgktydTQ71b15'])
+sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:2XVQdI3m0giGxNrwUhV3yP'])
