87151ecfe1091cabfe13358b978613891dd943d8
Vít Moulis <moulisvit@gmail.com>
2025-01-25 22:55:01 +0100

    gg

--- Code/spotifyTest.py
+++ Code/spotifyTest.py
@@ -1,21 +1,9 @@
-#!/usr/bin/env python
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 
-DEVICE_ID="35ccd4ae2cab8870610afd219d13b41e84688587"
-CLIENT_ID="5984261fa2d845b3bcf6463bb1df2c97"
-CLIENT_SECRET="9c2280c3c0ae4d9392a8870b90165b91"
+scope = "user-read-playback-state,user-modify-playback-state"
+sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
 
-# Spotify Authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                client_secret=CLIENT_SECRET,
-                                                redirect_uri="http://localhost:8080",
-                                                scope="user-read-playback-state,user-modify-playback-state"))
-
-
-# Transfer playback to the Raspberry Pi if music is playing on a different device
-sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
-
-# Play the spotify track at URI with ID 45vW6Apg3QwawKzBi03rgD (you can swap this for a diff song ID below)
-sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:2XVQdI3m0giGxNrwUhV3yP'])
+# Change track
+sp.start_playback(uris=['spotify:track:6gdLoMygLsgktydTQ71b15'])
