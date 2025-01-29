3e2a06893cec43755d6caa8af20b02b24949303d
Vít Moulis <moulisvit@gmail.com>
2025-01-29 17:43:04 +0100

    sek

--- Code/control.py
+++ Code/control.py
@@ -3,8 +3,9 @@ from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 
 DEVICE_ID = "35ccd4ae2cab8870610afd219d13b41e84688587"
-sp = spotipy.Spotify()
-#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-read-playback-state,user-modify-playback-state"))
+#sp = spotipy.Spotify()
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-read-playback-state,user-modify-playback-state"))
+what=""
 while(what!="end"):
     print("play; pause, next, end")
     what = input()
---
+++ Code/spotifyTest3.py
@@ -0,0 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from time import sleep
+sp_auth=spotipy.oauth2.SpotifyOAuth(secrets.sp_auth_id,
+                                secrets.sp_auth_pw, secrets.sp_callback_url,
+                                scope="user-read-playback-state,user-modify-playback-state", state=state)
+#url = sp_auth.get_authorize_url()
+auth_token=sp_auth.get_access_token(input())
+
+#{'access_token'  : 'BQD ... qE7K3PBZKB6iZFU3_4p', 
+# 'token_type'    : 'Bearer',
+# 'expires_in'    : 3600,
+# 'refresh_token' : 'AQCOS2Xo ... MK09ry7-a-fl61OwhuO1Q',
+# 'scope'         : 'playlist-modify-public user-library-read',
+# 'expires_at'    : 1548247835}
+
+sp = spotipy.Spotify(auth=auth_token)
+
+playlists = sp.current_user_playlists(limit=10)
+auth=auth_token['access_token']
