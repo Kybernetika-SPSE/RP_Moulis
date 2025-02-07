7790bf2e50916573aa20f56b591db8454d247bae
Vít Moulis <moulisvit@gmail.com>
2025-02-07 14:37:56 +0100

    code

---
+++ Code/code.py
@@ -0,0 +1,13 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from time import sleep
+DEVICE_ID = "35ccd4ae2cab8870610afd219d13b41e84688587"
+# Spotify Authentication
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(code="AQCO5lmamki1y2G-ZXqKrZCbOakKhQAFnj9g_X0finpn3_NUAcyKkyu4gYvex6MKBBPz_GREt1bdRHv1BbZovFlWTirItWudOWBb209RdaKq4nQMwOLw01yeS2MtgYP5Gb_zQ1ZOj5uHrmlgBYGD_k1m-9z6QEakGzyfdqFj2vrcZ1NLHIqqbk2OgmyZXJ6CyQksqMETMUhSsDjnlITjRJMTvhDj-lZEyw-G1LCYwv6fACh1wg0-fg",scope="user-read-playback-state,user-modify-playback-state"))
+
+
+
+
+# Transfer playback to the Raspberry Pi if music is playing on a different device
+sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
+sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:2XVQdI3m0giGxNrwUhV3yP'])
