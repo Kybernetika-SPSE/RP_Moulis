3ae1ee318fd4b2274766762d3e8cd4e05ee9057e
Vít Moulis <moulisvit@gmail.com>
2025-02-07 14:39:40 +0100

    code

--- Code/code.py
+++ Code/code.py
@@ -3,7 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 DEVICE_ID = "35ccd4ae2cab8870610afd219d13b41e84688587"
 # Spotify Authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(code="AQCO5lmamki1y2G-ZXqKrZCbOakKhQAFnj9g_X0finpn3_NUAcyKkyu4gYvex6MKBBPz_GREt1bdRHv1BbZovFlWTirItWudOWBb209RdaKq4nQMwOLw01yeS2MtgYP5Gb_zQ1ZOj5uHrmlgBYGD_k1m-9z6QEakGzyfdqFj2vrcZ1NLHIqqbk2OgmyZXJ6CyQksqMETMUhSsDjnlITjRJMTvhDj-lZEyw-G1LCYwv6fACh1wg0-fg",scope="user-read-playback-state,user-modify-playback-state"))
+code="AQCO5lmamki1y2G-ZXqKrZCbOakKhQAFnj9g_X0finpn3_NUAcyKkyu4gYvex6MKBBPz_GREt1bdRHv1BbZovFlWTirItWudOWBb209RdaKq4nQMwOLw01yeS2MtgYP5Gb_zQ1ZOj5uHrmlgBYGD_k1m-9z6QEakGzyfdqFj2vrcZ1NLHIqqbk2OgmyZXJ6CyQksqMETMUhSsDjnlITjRJMTvhDj-lZEyw-G1LCYwv6fACh1wg0-fg"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-read-playback-state,user-modify-playback-state"))
 
 
 
