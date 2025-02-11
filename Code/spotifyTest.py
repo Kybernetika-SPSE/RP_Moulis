1e02028ff49d73a9c9c48ab0531d27d0b1f38df4
Vít Moulis <moulisvit@gmail.com>
2025-02-11 20:38:59 +0100

    {{date}}

--- Code/spotifyTest.py
+++ Code/spotifyTest.py
@@ -11,9 +11,9 @@ DEVICE_ID = "edf62e2b191e93da0bfaee84694b175adbc719f5a"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri,scope="user-read-playback-state,user-modify-playback-state"))
 
  
-
+print(sp.devices())
 # Transfer playback to the Raspberry Pi if music is playing on a different device
-sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
+#sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
 #sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:2XVQdI3m0giGxNrwUhV3yP'])
 #sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:1Z5EUmtUOtCtjBCCrqJVP7'])
-sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:6LgJvl0Xdtc73RJ1mmpotq'])
+#sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:6LgJvl0Xdtc73RJ1mmpotq'])
