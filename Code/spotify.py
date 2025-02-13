98a1dd1e338d551bcbe64ed182aedab2a5443fc7
Vít Moulis <moulisvit@gmail.com>
2025-02-13 14:33:16 +0100

    global

--- Code/spotify.py
+++ Code/spotify.py
@@ -31,6 +31,9 @@ def long_string(display, text='', num_line=1, num_cols=16):
 
 
 def long_string_both(display, text1='', text2='', play=True, num_cols=16):
+    global refresh
+    global display_temp
+    global refresh_period
     lenght = max(len(text1),len(text2))
     if refresh == True:
         refresh = False
