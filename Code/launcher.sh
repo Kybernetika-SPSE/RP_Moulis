c1783db638542bd9abc3697a83b37afdc1159f56
Vít Moulis <moulisvit@gmail.com>
2025-02-14 17:18:14 +0100

    launcher

---
+++ Code/launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/mouli/RP_Moulis/Code
+sudo python spotify.py
+cd /
