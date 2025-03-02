d2902faced227c81e85d6b84aa5805acc43c9405
Vít Moulis <moulisvit@gmail.com>
2025-03-02 16:58:07 +0100

    launcher git sh merge

--- Code/git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /
-cd home/mouli/RP_Moulis
-sudo git reset --hard origin/main
-sudo git pull
-sudo git reset --hard origin/main
-cd /
--- Code/launcher.sh
+++ Code/launcher.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
-
+cd /
+cd home/mouli/RP_Moulis
+sudo git reset --hard origin/main
+sudo git pull
+sudo git reset --hard origin/main
 cd /
 cd home/mouli/RP_Moulis/Code
 sudo python spotify.py
