3f2aadabe1084514001a02113b0adbb86423041e
Vít Moulis <moulisvit@gmail.com>
2025-03-29 17:15:41 +0100

    ?

--- Code/soundcheck.py
+++ Code/soundcheck.py
@@ -1,30 +1,3 @@
-import sounddevice as sd
-import numpy as np
-
-# Nastavení parametrů
-DURATION = 2  # Délka snímání v sekundách
-SAMPLERATE = 44100  # Vzorkovací frekvence
-CHANNELS = 2  # Stereo výstup
-
-def check_audio_output():
-    print("Kontroluji zvuk...")
-
-    # Záznam signálu po dobu určitého intervalu
-    recording = sd.rec(int(DURATION * SAMPLERATE), samplerate=SAMPLERATE, channels=CHANNELS, dtype='float64')
-    sd.wait()  # Čeká na dokončení záznamu
-
-    # Analýza
-    signal_level = np.max(np.abs(recording))  # Maximální amplituda
-    if signal_level > 0.01:  # Limitní hodnota
-        print(f"Zvukový výstup detekován! Úroveň signálu: {signal_level}")
-        return True
-    else:
-        print("Zvukový výstup nezjištěn.")
-        return False
-
-# Spuštění funkce
-check_audio_output()
-
 import subprocess
 
 def check_connected_devices():
--- Code/spotify.py
+++ Code/spotify.py
@@ -263,7 +263,7 @@ while not io.input(26):
                     print(dev)
                     io.output(screen, True)
                     display.lcd_display_extended_string(line=1,string="{0x07} Bluetooth")
-                    display.lcd_display_string(line=2,string=dev[0])
+                    display.lcd_display_string(line=2,string=diakritika(dev[0]))
                     continue
                 else:
                     io.output(screen, False)
